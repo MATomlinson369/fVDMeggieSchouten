357435fb897675087a3774303db6940a3b87ec64
MATomlinson369 <152192076+MATomlinson369@users.noreply.github.com>
2024-09-30 15:12:52 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,13 @@
 // JavaScript Document
 console.log("hi");
 
+var button = document.querySelector("header nav button");
+
+var contentMenu = document.querySelector("header nav ul")
+
+
+button.onclick = hierIsMenu
+
+function hierIsMenu() {
+  contentMenu.classList.toggle("toonMenu");
+}
