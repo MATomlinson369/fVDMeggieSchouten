dcb17956e90ad4b529a7a9c2a25467e45edfeba7
MATomlinson369 <152192076+MATomlinson369@users.noreply.github.com>
2024-10-01 11:56:29 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,22 @@
 // JavaScript Document
 console.log("hi");
 
+
+var openbutton = document.querySelector("#open-menu");
+
+openbutton.addEventListener("click", openMenu)
+function openMenu() {
+    document.querySelector(".hamburger-nav").classList.add("toonMenu") 
+
+}
+
+var sluitbutton = document.querySelector("#sluit-menu");
+
+sluitbutton.addEventListener("click", sluitMenu)
+function sluitMenu() {
+    document.querySelector(".hamburger-nav").classList.remove("toonMenu") 
+
+}
+
+
+
